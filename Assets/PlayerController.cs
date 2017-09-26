0cafea81ae23b0b55512179979d57ed7a4bc3d59
Omer Dogan <omerdogan3@gmail.com>
2017-09-26 15:57:16 +0300

    day4

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	static bool isAlive = true;
-
-
+	
 	public float speed = 6; //character's speed
 	public float jumpForce = 10; //character's jump velocity
 	private bool isGround = false;
 	public LayerMask groundLayer;	
+	private Animator playerAnim;
 	private Rigidbody rb; //rigidbody component
-
+	private float tmp = 0.0f;
 	void Awake(){
 		rb = this.GetComponent<Rigidbody> ();
+		playerAnim = this.GetComponent<Animator>();
 	}
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
@@ -27,16 +27,28 @@ public class PlayerController : MonoBehaviour {
 
 	}
 
-
 	//Obje haraketliyse ve collider'i farkli objelerle temas halindeyse
 	//titreme gibi durumlardan uzak durmak icin FixedUpdate kullanilir.
 	void FixedUpdate(){
 		//Eger karakter canliysa && user tusa basiyorsa hareket ettir.
 
 		if (isAlive == true) {
-			if(Input.GetKey(KeyCode.Space) && isGrounded()){
-				Jump ();
+			if(isGrounded()){
+				if(Input.GetKey(KeyCode.Space)){
+				tmp = tmp + Time.deltaTime;
+				playerAnim.SetBool("jumpEnter", true);
+				if(tmp >=0.5f){
+					Jump ();	
+				}
+			 }
 			}
+			
+			if(Input.GetKeyUp(KeyCode.Space) || !isGrounded()){
+				playerAnim.SetBool("jumpEnter", false);
+				tmp = 0;
+			}
+
+			
 			if (Input.GetKey (KeyCode.RightArrow)) {
 				transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
 			} else if (Input.GetKey (KeyCode.LeftArrow)) {
