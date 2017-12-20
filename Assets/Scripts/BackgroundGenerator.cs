3f1e07c332741e886c24c410fd8aaf59eb08d4ab
Mert Avcioglu <34686131+mertavcioglu@users.noreply.github.com>
2017-12-20 13:05:42 +0300

    Background and Water Generator

---
+++ Assets/Scripts/BackgroundGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundGenerator : MonoBehaviour {
+
+	public int backgroundSpawnTime;
+
+	public GameObject[] backgroundArray;
+	public List<GameObject> inScene;
+
+	private int counter=30;
+	private int spawnedObjNum = 0;
+
+	public GameObject playerObj;
+	private Player player;
+
+	void Awake(){
+		player = playerObj.GetComponent<Player> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("backgroundSpawn", 0, backgroundSpawnTime);
+		//InvokeRepeating ("Destroyer", 5, backgroundSpawnTime);
+	}
+
+	void backgroundSpawn(){
+		float randX = Random.Range (60, 80);
+		int randomBackground = (int)Random.Range (0, backgroundArray.Length);
+		transform.Translate(new Vector3(randX,0,0));
+		Vector3 pos = new Vector3 (this.transform.position.x,  this.transform.position.y, 0);
+		GameObject tmpObj = Instantiate (backgroundArray [randomBackground], pos, transform.rotation) as GameObject;
+		tmpObj.transform.SetParent (GameObject.Find ("BackgroundGameObject").transform);
+		++spawnedObjNum;
+		inScene.Add (tmpObj);
+	}
+
+	void Destroyer(){
+		if (player.isAlive) {
+			GameObject tmp = inScene[spawnedObjNum-1];
+			Destroy (tmp,counter);
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/WaterGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGenerator : MonoBehaviour {
+
+	public GameObject waterObj;
+	public int waterSpawnTime;
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		//waterSpawn ();
+		//InvokeRepeating ("waterSpawn", 0, waterSpawnTime);
+	}
+	/*
+	void waterSpawn(){
+		transform.Translate(new Vector3(100,0,0));
+		Vector3 pos = new Vector3 (this.transform.position.x,  this.transform.position.y, this.transform.position.z);
+		GameObject waterIns = Instantiate (waterObj, pos, transform.rotation) as GameObject;
+	}*/
+
+	void Update(){
+		
+		transform.position = new Vector3 (player.transform.position.x, 0f, 0f);
+	}
+
+}
